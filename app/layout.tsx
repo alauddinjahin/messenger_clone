186b19987f799848cd1c332ecf342fbd72792a9b
alauddinjahin <mdalauddinjahin365@gmail.com>
2023-05-18 16:31:44 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
+
+export const metadata = {
+  title: 'Messenger App',
+  description: 'Realtime chatbox',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
